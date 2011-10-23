fd85fa432b7feaeb0b55e1a189e983439809ad2f
Yann Sionneau <yann@minet.net>
2011-10-24 01:46:06 +0200

    gui: listener_thread implemented

--- gui/Window.cpp
+++ gui/Window.cpp
@@ -4,6 +4,39 @@
 
 const char *machines[] = {"machine1", "machine2", "machine3"};
 
+#define READ_PIPE       0
+#define WRITE_PIPE      1
+
+/* This is the listener thread */
+void *Window::listener(void *obj)
+{
+	Window *win = (Window *)obj;
+	printf("Listener thread : RUNNING...\n");
+	ssize_t new_size;
+	ssize_t size;
+	int integer;
+	
+	while (1) {
+		size = 0;
+		do {
+			new_size = read(win->pipe_fd[READ_PIPE], &integer, sizeof(integer));
+
+			if (new_size == 0) {
+				printf("End of file\n");
+				exit(1);
+			} else if (new_size == -1) {
+				perror("read from pipe:");
+				exit(1);
+			}
+			size += new_size;
+		} while (size < sizeof(integer));
+
+		printf("listener_thread: integer %d read from pipe\n", integer);
+	}
+
+	return obj;
+}
+
 void Window::addMachine(int num)
 {
 	
